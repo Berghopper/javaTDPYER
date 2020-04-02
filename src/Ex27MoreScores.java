ca119ae52f4ee1e6a3014cf72cfc10a6f1c9ea6a
Casper <12167073+Berghopper@users.noreply.github.com>
2020-04-02 17:05:04 +0200

    Finished Ex27 (MoreScores)

--- src/Ex27MoreScores.java
+++ src/Ex27MoreScores.java
@@ -1,5 +1,18 @@
+import java.util.Random;
+
 public class Ex27MoreScores {
     public static void main(String[] args) {
-        // still busy
+        Random generator = new Random();
+        int[] testscoresArray = new int[20];
+        for (int i = 0; i < 20; i++) {
+            testscoresArray[i] = generator.nextInt(99)+1;
+        }
+        int counter = 0;
+        for (int score : testscoresArray) {
+            if (score >= 85 && score <= 90) {
+                counter++;
+            }
+        }
+        System.out.println(counter);
     }
 }
