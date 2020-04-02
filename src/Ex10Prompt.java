b830a8ea25b4da9bf46105352ba6bdbd69c9ab58
Casper <12167073+Berghopper@users.noreply.github.com>
2020-04-02 14:46:53 +0200

    Finished Ex10 (prompt)

---
+++ src/Ex10Prompt.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ex10Prompt {
+    public static void main(String[] args) {
+        System.out.println("Promter!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Are you sure you want to quit [Y,N]? ");
+        String input = scanner.nextLine();
+        switch (input) {
+            case "Y":
+                System.out.println("Continuing program!");
+                break;
+            case "N":
+                System.out.println("Stopping program!");
+                break;
+            default:
+                System.out.println("What do you mean?...");
+                throw new java.lang.RuntimeException("Great you crashed it again...");
+        }
+    }
+}
