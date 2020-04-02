9c6bc9b975c10f4208e84400f103f20f6b990378
Casper <12167073+Berghopper@users.noreply.github.com>
2020-04-02 15:35:00 +0200

    Finished Ex21 (weirdcalculator)

---
+++ src/Ex21WeirdAddition.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ex21WeirdAddition {
+    public static void main(String[] args) {
+        System.out.println("Weird calculator!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive int, I will add each single digit! (e.g. 3582=18): ");
+        String input = scanner.nextLine().strip();
+        int total = 0;
+        for (int i = 0 ; i < input.length(); i++) {
+            int thenumber = Integer.parseInt(input.substring(i,i+1));
+            total += thenumber;
+        }
+        System.out.printf("Answer = %d", total);
+    }
+}
