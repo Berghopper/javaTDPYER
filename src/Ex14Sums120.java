bea61a6883b9db250d738cac924c092df60f2790
Casper <12167073+Berghopper@users.noreply.github.com>
2020-04-02 14:55:25 +0200

    Finished Ex14 (sums120)

---
+++ src/Ex14Sums120.java
@@ -0,0 +1,9 @@
+public class Ex14Sums120 {
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 1; i < 21; i++) {
+            total += i;
+            System.out.println(total);
+        }
+    }
+}
