19865b1b5ca54dd9b40f4a61b92fec528a98463f
Casper <12167073+Berghopper@users.noreply.github.com>
2020-04-02 15:03:27 +0200

    Finished Ex16 (sequence)

--- src/Ex16DisplaySequence.java
+++ src/Ex16DisplaySequence.java
@@ -1,2 +1,9 @@
 public class Ex16DisplaySequence {
+    public static void main(String[] args) {
+        int j = 5;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(j+(4*i));
+        }
+    }
+
 }
