b43472e665570f2dd6b1f821c02a8d15d43b75c5
Casper <12167073+Berghopper@users.noreply.github.com>
2020-04-03 12:32:04 +0200

    Basic functionality done

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -11,6 +11,10 @@ public class Calculator extends JFrame implements ActionListener {
     String operator;
     int firstNumber;
     int secondNumber;
+    String firstNumberRaw = "";
+    String secondNumberRaw = "";
+    boolean firstNumberEntered = false;
+    boolean secondNumberEntered = false;
 
     public Calculator() {
         String[] labels = {
@@ -43,9 +47,75 @@ public class Calculator extends JFrame implements ActionListener {
         calculator.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+    public void parsePressedButtons(String theButton) {
+        switch(theButton) {
+            // num
+            case "0": case "1": case "2": case "3": case "4":
+            case "5": case "6": case "7": case "8": case "9":
+                if (firstNumberEntered) {
+                    this.secondNumberEntered = true;
+                    this.secondNumberRaw += theButton;
+                } else {
+                    this.firstNumberRaw += theButton;
+                }
+
+                break;
+            // save operator
+            case "/": case "*": case "+": case "-":
+                this.operator = theButton;
+                if (!this.firstNumberRaw.equals("")) {
+                    this.firstNumberEntered = true;
+                }
+                break;
+            // clear
+            case "C":
+                resetAllStates();
+                break;
+            // calc
+            case "=":
+                if (this.firstNumberEntered && this.secondNumberEntered && !this.operator.equals("")) {
+                    this.firstNumber = Integer.parseInt(this.firstNumberRaw);
+                    this.secondNumber = Integer.parseInt(this.secondNumberRaw);
+                    switch (this.operator) {
+                        case "/":
+                            System.out.printf("Result of %d / %d =\n", this.firstNumber, this.secondNumber);
+                            System.out.println(this.firstNumber/this.secondNumber);
+                            break;
+                        case "*":
+                            System.out.printf("Result of %d * %d =\n", this.firstNumber, this.secondNumber);
+                            System.out.println(this.firstNumber*this.secondNumber);
+                            break;
+                        case "+":
+                            System.out.printf("Result of %d + %d =\n", this.firstNumber, this.secondNumber);
+                            System.out.println(this.firstNumber+this.secondNumber);
+                            break;
+                        case "-":
+                            System.out.printf("Result of %d - %d =\n", this.firstNumber, this.secondNumber);
+                            System.out.println(this.firstNumber-this.secondNumber);
+                            break;
+                        default:
+                            System.out.println("Undefined operator!");
+                    }
+                }
+                resetAllStates();
+                break;
+            default:
+                System.out.println("Fatal error, you pressed a button that does not exist?");
+        }
+    }
+
+    public void resetAllStates() {
+        this.firstNumberRaw = "";
+        this.secondNumberRaw = "";
+        this.firstNumberEntered = false;
+        this.secondNumberEntered = false;
+        this.operator = "";
+    }
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
         String currentButton = e.getActionCommand();
-        System.out.println(currentButton);
+        parsePressedButtons(currentButton);
     }
 }
