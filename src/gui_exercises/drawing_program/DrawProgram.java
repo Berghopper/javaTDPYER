53c62ff12939b4c52bf95bb77c69eb173ca634c9
Casper <12167073+Berghopper@users.noreply.github.com>
2020-04-08 11:55:31 +0200

    Small refactor

--- src/gui_exercises/drawing_program/DrawProgram.java
+++ src/gui_exercises/drawing_program/DrawProgram.java
@@ -5,17 +5,104 @@ import gui_exercises.drawing_program.shapes.Rectangle;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 import java.util.ArrayList;
 
-public class DrawProgram extends JFrame implements MouseListener {
+public class DrawProgram extends JFrame {
 
-    ArrayList<Figure> figures = new ArrayList<>();
-    Figure figure = new Oval();
+    DrawingPanel drawingPanel = new DrawingPanel();
+//    private JLayeredPane layeredPane;
 
     public DrawProgram() throws HeadlessException {
-        addMouseListener(this);
+//        setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
+//        layeredPane = new JLayeredPane();
+//        layeredPane.setPreferredSize(new Dimension(300, 310));
+//        layeredPane.setBorder(BorderFactory.createTitledBorder(
+//                "Drawing pane"));
+        // MENU===
+        //Where the GUI is created:
+        JMenuBar menuBar;
+        JMenu menu, submenu;
+        JMenuItem menuItem;
+        JRadioButtonMenuItem rbMenuItem;
+        JCheckBoxMenuItem cbMenuItem;
+
+//Create the menu bar.
+        menuBar = new JMenuBar();
+
+//Build the first menu.
+        menu = new JMenu("A Menu");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "The only menu in this program that has menu items");
+        menuBar.add(menu);
+
+//a group of JMenuItems
+        menuItem = new JMenuItem("A text-only menu item",
+                KeyEvent.VK_T);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_1, InputEvent.ALT_DOWN_MASK));
+        menuItem.getAccessibleContext().setAccessibleDescription(
+                "This doesn't really do anything");
+        menu.add(menuItem);
+
+        menuItem = new JMenuItem("Both text and icon",
+                new ImageIcon("images/middle.gif"));
+        menuItem.setMnemonic(KeyEvent.VK_B);
+        menu.add(menuItem);
+
+        menuItem = new JMenuItem(new ImageIcon("images/middle.gif"));
+        menuItem.setMnemonic(KeyEvent.VK_D);
+        menu.add(menuItem);
+
+//a group of radio button menu items
+        menu.addSeparator();
+        ButtonGroup group = new ButtonGroup();
+        rbMenuItem = new JRadioButtonMenuItem("A radio button menu item");
+        rbMenuItem.setSelected(true);
+        rbMenuItem.setMnemonic(KeyEvent.VK_R);
+        group.add(rbMenuItem);
+        menu.add(rbMenuItem);
+
+        rbMenuItem = new JRadioButtonMenuItem("Another one");
+        rbMenuItem.setMnemonic(KeyEvent.VK_O);
+        group.add(rbMenuItem);
+        menu.add(rbMenuItem);
+
+//a group of check box menu items
+        menu.addSeparator();
+        cbMenuItem = new JCheckBoxMenuItem("A check box menu item");
+        cbMenuItem.setMnemonic(KeyEvent.VK_C);
+        menu.add(cbMenuItem);
+
+        cbMenuItem = new JCheckBoxMenuItem("Another one");
+        cbMenuItem.setMnemonic(KeyEvent.VK_H);
+        menu.add(cbMenuItem);
+
+//a submenu
+        menu.addSeparator();
+        submenu = new JMenu("A submenu");
+        submenu.setMnemonic(KeyEvent.VK_S);
+
+        menuItem = new JMenuItem("An item in the submenu");
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_2, InputEvent.ALT_DOWN_MASK));
+        submenu.add(menuItem);
+
+        menuItem = new JMenuItem("Another item");
+        submenu.add(menuItem);
+        menu.add(submenu);
+
+//Build second menu in the menu bar.
+        menu = new JMenu("Another Menu");
+        menu.setMnemonic(KeyEvent.VK_N);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "This menu does nothing");
+        menuBar.add(menu);
+        this.setJMenuBar(menuBar);
+        // END MENU===
+        drawingPanel.setBounds(0,0,300,200);
+        getContentPane().add(drawingPanel);
     }
 
     public static void main(String[] args) {
@@ -25,9 +112,21 @@ public class DrawProgram extends JFrame implements MouseListener {
         drawProgram.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+
+}
+
+class DrawingPanel extends JPanel implements MouseListener{
+    ArrayList<Figure> figures = new ArrayList<>();
+    Figure figure = new Oval();
+
+    public DrawingPanel(){
+        this.addMouseListener(this);
+    }
+
     @Override
     public void paint(Graphics g) {
         super.paint(g);
+        g.drawString("A text!",30,30);
         for (Figure f : figures) {
             f.draw(g);
         }
@@ -40,6 +139,7 @@ public class DrawProgram extends JFrame implements MouseListener {
 
     @Override
     public void mousePressed(MouseEvent e) {
+        System.out.println("mouse pressed!");
         figure = new Rectangle();
         figure.x1 = e.getX();
         figure.y1 = e.getY();
