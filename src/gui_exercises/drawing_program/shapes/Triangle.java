144cc63447bf20b6cf05689233965209f2c2cd9b
Casper <12167073+Berghopper@users.noreply.github.com>
2020-04-08 14:01:38 +0200

    Fixed triangle (static triangle)

--- src/gui_exercises/drawing_program/shapes/Triangle.java
+++ src/gui_exercises/drawing_program/shapes/Triangle.java
@@ -5,9 +5,9 @@ import java.awt.Graphics;
 public class Triangle extends Figure {
     @Override
     public void draw(Graphics g) {
-        // TODO fix this mess (store more points to make life easier)
-        g.drawLine(x1,y2,x2,y2);
-        g.drawLine(x2,y2,x2/2,y1);
-        g.drawLine(x1,y2,x2/2,y2);
+        // TODO add freehand triangle
+        g.drawLine(x1,y1,x2,y1);
+        g.drawLine(x1,y1,x1,y2);
+        g.drawLine(x1,y2,x2,y1);
     }
 }
